a3242489a8bb036cbe15c44b4da6a4c57cf8b08b
HasshamNadeem <hassham.nadeem@dubizzlelabs.com>
2023-07-25 11:44:36 +0500

    User Prefix, Eloquent Resource Pagination,lazy loading, timeststamps in pivot table

--- database/migrations/2023_07_20_092520_create_product_user_table.php
+++ database/migrations/2023_07_20_092520_create_product_user_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
 
             $table->unsignedBigInteger('user_id')->index();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
         });
     }
 
